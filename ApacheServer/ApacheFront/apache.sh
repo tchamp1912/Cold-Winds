fe03e5f8428766c186c433462790a765d52fbbde
Thomas Campion <campion.t@husky.neu.edu>
2019-07-21 11:10:21 -0400

    Initial additions

---
+++ ApacheServer/ApacheFront/apache.sh
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf400
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red239\green237\blue236;\red148\green108\blue71;
+\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c94902\c94510\c94118;\cssrgb\c65098\c49804\c34902;
+\cssrgb\c100000\c100000\c100000\c49804;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl400\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 sudo apt\cf4 \cb5 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 -\cf2 \cb3 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 get update\
+}
